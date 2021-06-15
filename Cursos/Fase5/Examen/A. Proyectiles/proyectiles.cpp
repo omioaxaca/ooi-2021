b70a9a5193041e23a4037bd9655fcc67668803a6
Daniel Gomez Antonio <dannyel.2511@gmail.com>
2021-06-15 18:34:34 -0500

    Clarificar proyectiles

--- Cursos/Fase5/Examen/A. Proyectiles/proyectiles.cpp
+++ Cursos/Fase5/Examen/A. Proyectiles/proyectiles.cpp
@@ -8,26 +8,31 @@
 
 using namespace std;
 
-vector<vector<long long int>> proyectiles;
+// Crear un vector de vectores.
+// El vector interior se usa para almacenar dos valores, los valores de x y y.
+vector<vector<int>> proyectiles;
+// Definir constantes para representar la coordenadas X y Y dentro del vector
+const int X = 0;
+const int Y = 1;
 
 
-bool busqBin(vector<int> nuevo) {
+bool busqBin(vector<int> proyectil) {
     int ini = 0;
     int fin = proyectiles.size() - 1;
     while (ini <= fin) {
         int mit = ini + (fin - ini) / 2;
-        if (proyectiles[mit][0] == nuevo[0]) {
-            if (proyectiles[mit][1] == nuevo[1]) {
+        if (proyectiles[mit][X] == proyectil[X]) {
+            if (proyectiles[mit][Y] == proyectil[Y]) {
                 return true;
             }
-            else if(proyectiles[mit][1] > nuevo[1]) {
+            else if(proyectiles[mit][Y] > proyectil[Y]) {
                 fin = mit - 1; 
             }
             else {
                 ini = mit + 1;
             }
         }
-        else if(proyectiles[mit][0] > nuevo[0]) {
+        else if(proyectiles[mit][X] > proyectil[X]) {
             fin = mit - 1;
         }
         else {
@@ -45,20 +50,34 @@ int main() {
     // Crear un vector de 2 posiciones para representar al proyectil
     // Indice 0: Representa x
     // Indice 1: Representa y
-    vector<long long int> proyectil(2);
+    vector<int> proyectil(2);
     for (int i = 0; i < n; i++) {
-        cin >> proyectil[0] >> proyectil[1];
+        cin >> proyectil[X] >> proyectil[Y];
         // Insertar el proyectil en el vector de proyectiles
         proyectiles.push_back(proyectil);
     }
 
-    // Ordenar primero por x y luego por y
+    // Ordenar el vector, por defecto esto toma el orden lexicografico de cada vector interno
+    // Es decir, si te tiene un vector asi:
+    // {
+    //     {1, 3},
+    //     {5, 8},
+    //     {1, 2},
+    //     {3, 9}
+    // }
+    // Al ser ordenado quedara:
+    // {
+    //     {1, 2},
+    //     {1, 3},
+    //     {3, 9},
+    //     {5, 8}
+    // }
     sort(proyectiles.begin(), proyectiles.end());
     
+    // Leer cada proyectil y buscar si ya existe en nuestro vector
     for (int i = 0; i < k; i++) {
-        vector<int> nuevo(2);
-        cin >> nuevo[0] >> nuevo[1];
-        if (busqBin(nuevo) == true) {
+        cin >> proyectil[X] >> proyectil[Y];
+        if (busqBin(proyectil) == true) {
             cout << "0\n";
         }
         else {
